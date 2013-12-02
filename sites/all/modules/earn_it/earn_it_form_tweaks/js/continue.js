832d3787d9bb126e8adc026c6397fbe158fd51dc
sdfitzpa <sdfitzpa@utoadmins-MacBook-Pro-2.local>
2013-12-02 09:54:02 -0700

    trying to set up an endpoint to update user progress

--- sites/all/modules/earn_it/earn_it_form_tweaks/js/continue.js
+++ sites/all/modules/earn_it/earn_it_form_tweaks/js/continue.js
@@ -2,12 +2,59 @@
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
     
+      function getBaseURL() {
+        var url = location.href;  // entire url including querystring - also: window.location.href;
+        var baseURL = url.substring(0, url.indexOf('/', 14));
+ 
+ 
+        if (baseURL.indexOf('http://localhost') != -1) {
+          // Base Url for localhost
+          var url = location.href;  // window.location.href;
+          var pathname = location.pathname;  // window.location.pathname;
+          var index1 = url.indexOf(pathname);
+          var index2 = url.indexOf("/", index1 + 1);
+          var baseLocalUrl = url.substr(0, index2);
+ 
+          return baseLocalUrl + "/";
+        }
+        else {
+          // Root Url for domain name
+          return baseURL + "/";
+        }
+      }
+      
+      var theURL = getBaseURL();
+      
+      function handleClick() {
+ 
+        if (window.event.srcElement.id == "panels-ipe-paneid-10") {
+          goToWeekOne();
+
+        }
+      }
+
+      
+      document.onclick = handleClick;
+    
       function goToWeekOne() {
+        global $user;
+      
+        $.get( 
+              theURL + "user_progress/"+ $user->uid + "/" + 2,
+              function(data) {
+                if (data.form_errors) {
+                  alert("error");
+                }
+                else {
+                  alert("success");
+                }
+              }
+            );
         // FIXME: actually have this do something
         alert("continue clicked");
       }
       
-      document.getElementById("panels-ipe-paneid-10").innerHTML = "<button onClick='goToWeekOne()'>Continue</button>"      
+      document.getElementById("panels-ipe-paneid-10").innerHTML = "<button>Continue</button>"      
     }
   };
 })(jQuery);
