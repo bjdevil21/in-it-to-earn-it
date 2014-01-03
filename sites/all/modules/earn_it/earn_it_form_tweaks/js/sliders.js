23b0996e2f92911be7bf45ffd0dc5824e37a6852
sdfitzpa <sdfitzpa@utoadmins-MacBook-Pro-2.local>
2014-01-03 13:25:34 -0700

    first pass at demo code to see if I can grab all the divs I need to add listeners to sliders

---
+++ sites/all/modules/earn_it/earn_it_form_tweaks/js/sliders.js
@@ -0,0 +1,39 @@
+(function($) {
+  Drupal.behaviors.myBehavior = {
+    attach: function (context, settings) {
+    
+      function getBaseURL() {
+        var url = location.href;  // entire url including querystring - also: window.location.href;
+        var baseURL = url.substring(0, url.indexOf('/', 14));
+ 
+ 
+        if (baseURL.indexOf('http://localhost') != -1) {
+          // Base Url for localhost
+          var url = location.href;  // window.location.href;
+          var pathname = location.pathname;  // window.location.pathname;
+          var index1 = url.indexOf(pathname);
+          var index2 = url.indexOf("/", index1 + 1);
+          var baseLocalUrl = url.substr(0, index2);
+ 
+          return baseLocalUrl + "/";
+        }
+        else {
+          // Root Url for domain name
+          return baseURL + "/";
+        }
+      }
+      
+      var theURL = getBaseURL();
+      
+      function addSliderEvents() {
+        var sliders = document.getElementsByClassName('hoursslider');
+        for(var i=0; i<sliders.length; i++) { 
+          console.log(sliders[i]);
+        }
+      }
+      
+      
+      window.onload=addSliderEvents();
+    }
+  };
+})(jQuery);
