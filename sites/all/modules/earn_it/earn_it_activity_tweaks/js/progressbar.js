5f5f84ffc2accd3a7746fabb4cf4bd2ea35aeba6
sdfitzpa <sdfitzpa@utoadmins-MacBook-Pro-2.local>
2014-01-24 16:13:56 -0700

    trying a different thing to get the js to work

--- sites/all/modules/earn_it/earn_it_activity_tweaks/js/progressbar.js
+++ sites/all/modules/earn_it/earn_it_activity_tweaks/js/progressbar.js
@@ -1,12 +1,18 @@
 (function($) {
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-    
-      $(function() {
-        $( "#progressbar" ).progressbar({
-          value: 37
+      
+      $.getScript("http://code.jquery.com/ui/1.10.4/jquery-ui.js", function(){
+
+        $(function() {
+          $( "#progressbar" ).progressbar({
+            value: 37
+          });
         });
+      
       });
+      
+      
     }
   };
 })(jQuery);
