48f10cdcde7a80f18a6c107c63d84b067f1630e5
sdfitzpa <sdfitzpa@utoadmins-MacBook-Pro-2.local>
2014-01-24 16:17:59 -0700

    trying to trick it into believing that js is hosted on https

--- sites/all/modules/earn_it/earn_it_activity_tweaks/js/progressbar.js
+++ sites/all/modules/earn_it/earn_it_activity_tweaks/js/progressbar.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
       
-      $.getScript("http://code.jquery.com/ui/1.10.4/jquery-ui.js", function(){
+      $.getScript("https://code.jquery.com/ui/1.10.4/jquery-ui.js", function(){
 
         $(function() {
           $( "#progressbar" ).progressbar({
