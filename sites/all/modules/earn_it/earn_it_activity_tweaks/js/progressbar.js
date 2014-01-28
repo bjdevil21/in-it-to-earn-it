dbfead2a9849823085474cdcffce1a1b74a525e4
sdfitzpa <sdfitzpa@utoadmins-MacBook-Pro-2.local>
2014-01-27 18:44:35 -0700

    changing my progressbar.js function back to not call the ui from elsewhere

--- sites/all/modules/earn_it/earn_it_activity_tweaks/js/progressbar.js
+++ sites/all/modules/earn_it/earn_it_activity_tweaks/js/progressbar.js
@@ -1,16 +1,12 @@
 (function($) {
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-      
-      $.getScript("https://code.jquery.com/ui/1.10.4/jquery-ui.js", function(){
 
         $(function() {
           $( "#progressbar" ).progressbar({
             value: 37
           });
         });
-      
-      });
       
       
     }
