eaecf2e1022e828d5e720db423c4ad0decf61620
Bryan <aubjr@asu.edu>
2014-10-21 18:47:30 -0700

    Cleanup of form tweaks module and added documentation

--- sites/all/modules/earn_it/earn_it_form_tweaks/js/continue.js
+++ sites/all/modules/earn_it/earn_it_form_tweaks/js/continue.js
@@ -5,8 +5,7 @@
       function getBaseURL() {
         var url = location.href;  // entire url including querystring - also: window.location.href;
         var baseURL = url.substring(0, url.indexOf('/', 14));
- 
- 
+
         if (baseURL.indexOf('http://localhost') != -1) {
           // Base Url for localhost
           var url = location.href;  // window.location.href;
